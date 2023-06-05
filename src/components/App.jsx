2a322669c176dbd5a4bb80df7ea96c3a880a4f5a
VladSurzhko <surzhkovlad69@gmail.com>
2023-06-05 20:54:43 +0200

    uuuuuup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-import Feedbacks from "./feedback/feedback.jsx"
+import Feedback from "./feedback/feedback"
+
 
 
 export const App = () => {
   return (
-    <div>
-      <Feedbacks
-        state={{
-          good: 0,
-          neutral: 0,
-          bad: 0
-        }}
-    />
+    <div
+      style={{
+        padding: '60px',
+        backgroundColor:" azure", 
+        height: '100vh',
+        // display: 'flex',
+        fontSize: 50,
+        // color: '#010101'
+      }}
+    >
+      < Feedback
+      />      
     </div>
   );
 };
+
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Feedbacks
+//         state={{
+//           good: 0,
+//           neutral: 0,
+//           bad: 0
+//         }}
+//     />
+//     </div>
+//   );
+// };
